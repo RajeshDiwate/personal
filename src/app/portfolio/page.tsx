f11542aa9dd26e81abdc29df2ceff7e1cf25eb49
Your Name <your.email@example.com>
2025-01-09 00:53:01 +0530

    Portfolio Changes

--- src/app/portfolio/page.tsx
+++ src/app/portfolio/page.tsx
@@ -52,7 +52,7 @@ const PortfolioPage = () => {
       animate={{ y: "0%" }}
       transition={{ duration: 1 }}
     >
-      <div className="h-fit relative" ref={ref as any}>
+      <div className="h-[600vh] relative" ref={ref as any}>
         <div className="w-screen h-[calc(100vh-6rem)] flex items-center justify-center text-8xl text-center">
           My Works
         </div>
